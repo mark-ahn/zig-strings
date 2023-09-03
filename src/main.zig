fcad2a9de5440dfb6796f5c4869321138b8efb27
mark.ahn <ahn8202@gmail.com>
2023-09-03 16:36:01 +0900

    adds toLower, toUpper

--- src/main.zig
+++ src/main.zig
@@ -5,34 +5,57 @@ pub fn hasPrefix(s: []const u8, prefix: []const u8) bool {
     if (s.len < prefix.len) return false;
     return std.mem.eql(u8, s[0..prefix.len], prefix);
 }
+test "hasPrefix" {
+    try testing.expect(hasPrefix("some-strings-value", "some"));
+}
 
 pub fn hasSuffix(s: []const u8, suffix: []const u8) bool {
     if (s.len < suffix.len) return false;
     return std.mem.eql(u8, s[s.len - suffix.len ..], suffix);
+}
+test "hasSuffix" {
+    try testing.expect(hasSuffix("some-strings-value", "value"));
 }
 
 pub fn trimPrefix(s: []const u8, prefix: []const u8) []const u8 {
     if (!hasPrefix(s, prefix)) return s;
     return s[prefix.len..];
 }
+test "trimPrefix" {
+    try testing.expect(std.mem.eql(u8, trimPrefix("some-strings-value", "some"), "-strings-value"));
+}
 
 pub fn trimSuffix(s: []const u8, suffix: []const u8) []const u8 {
     if (!hasSuffix(s, suffix)) return s;
     return s[0..(s.len - suffix.len)];
 }
-
-test "hasPrefix" {
-    try testing.expect(hasPrefix("some-strings-value", "some"));
-}
-
-test "hasSuffix" {
-    try testing.expect(hasSuffix("some-strings-value", "value"));
-}
-
-test "trimPrefix" {
-    try testing.expect(std.mem.eql(u8, trimPrefix("some-strings-value", "some"), "-strings-value"));
-}
-
 test "trimSuffix" {
     try testing.expect(std.mem.eql(u8, trimSuffix("some-strings-value", "value"), "some-strings-"));
+}
+
+pub fn toLower(allocator: std.mem.Allocator, s: []const u8) ![]u8 {
+    var v = try allocator.alloc(u8, s.len);
+    for (s, 0..) |c, i| {
+        v[i] = std.ascii.toLower(c);
+    }
+    return v;
+}
+test toLower {
+    const str = try toLower(testing.allocator, "SOME_UPPER_CASE");
+    defer testing.allocator.free(str);
+    try testing.expect(std.mem.eql(u8, str, "some_upper_case"));
+}
+
+pub fn toUpper(allocator: std.mem.Allocator, s: []const u8) ![]u8 {
+    var v = try allocator.alloc(u8, s.len);
+    for (s, 0..) |c, i| {
+        v[i] = std.ascii.toUpper(c);
+    }
+    return v;
+}
+
+test toUpper {
+    const str = try toUpper(testing.allocator, "some_upper_case");
+    defer testing.allocator.free(str);
+    try testing.expect(std.mem.eql(u8, str, "SOME_UPPER_CASE"));
 }
