02362e234c532b2f879c204678154fcad4eac01b
mark.ahn <ahn8202@gmail.com>
2023-09-03 12:43:42 +0900

    init

---
+++ src/main.zig
@@ -0,0 +1,20 @@
+const std = @import("std");
+const testing = std.testing;
+
+fn hasPrefix(s: []const u8, prefix: []const u8) bool {
+    if (s.len < prefix.len) return false;
+    return std.mem.eql(u8, s[0..prefix.len], prefix);
+}
+
+fn hasSuffix(s: []const u8, suffix: []const u8) bool {
+    if (s.len < suffix.len) return false;
+    return std.mem.eql(u8, s[s.len - suffix.len ..], suffix);
+}
+
+test "hasPrefix" {
+    try testing.expect(hasPrefix("some-strings-value", "some"));
+}
+
+test "hasSuffix" {
+    try testing.expect(hasSuffix("some-strings-value", "value"));
+}
